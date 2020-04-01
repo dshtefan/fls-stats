e65460843bbcbea1e30be0fd4e318f9ad080868a
Denis <den.shtefan@gmail.com>
2020-04-01 18:40:30 +0300

    Add a table-component

--- src/components/ResultTable/ResultTable.tsx
+++ src/components/ResultTable/ResultTable.tsx
@@ -3,8 +3,33 @@ import './ResultTable.css'
 
 const ResultTable = () => {
   return (
-     <div>
-
+     <div className="result-table">
+        <table>
+           <tr>
+              <th>Product</th>
+              <th>Min Usage</th>
+              <th>Max Usage</th>
+              <th>Average Usage</th>
+           </tr>
+           <tr>
+              <td>IntelliJ IDEA</td>
+              <td>asa</td>
+              <td>asa</td>
+              <td>asa</td>
+           </tr>
+           <tr>
+              <td>WebStorm</td>
+              <td>3r3er</td>
+              <td>3r3er</td>
+              <td>3r3er</td>
+           </tr>
+           <tr>
+              <td>PHPStorm</td>
+              <td>3r3er</td>
+              <td>3r3er</td>
+              <td>3r3er</td>
+           </tr>
+        </table>
      </div>
   )
 };
